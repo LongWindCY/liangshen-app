3b65c96979c2932f2da58dbe6f852e0332f3c958
王春成 <673047374@qq.com>
2018-09-23 16:35:12 +0800

    头部、侧边导航栏居中

--- src/App.js
+++ src/App.js
@@ -24,68 +24,71 @@ class App extends React.Component {
                     trigger={null}
                     collapsible
                     collapsed={this.state.collapsed}
-                    style={{position: 'fixed', height: '100%'}}
+                    style={{ position: 'fixed', height: '100%' }}
                 >
-                    <div className="logo"
-                         style={{width: '120px',
-                             height: '31px',
-                             background: 'rgba(255,255,255,.2)',
-                             margin: '16px 24px 16px 0',
-                             float: 'left'}} />
-                    <Menu theme="dark"
-                          mode="inline"
-                          // defaultSelectedKeys={['1']}
-                    >
-                        <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span>个人信息</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="shopping-cart"/>
-                            <span>购物车</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="home"/>
-                            <span>收货地址</span>
-                        </Menu.Item>
-                    </Menu>
+                    
+                    <div className="leftbar">
+                        <Menu theme="dark"
+                            mode="inline"
+                        // defaultSelectedKeys={['1']}
+                        >
+                            <Menu.Item key="1">
+                                <Icon type="user" />
+                                <span>个人信息</span>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Icon type="shopping-cart" />
+                                <span>购物车</span>
+                            </Menu.Item>
+                            <Menu.Item key="3">
+                                <Icon type="home" />
+                                <span>收货地址</span>
+                            </Menu.Item>
+                        </Menu>
+                    </div>
                 </Sider>
                 <Layout>
                     <Header style={{ background: '#fff', padding: 0 }}>
                         <Layout>
                             <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
                                 <div className="logo"
-                                    style={{width: '120px',
+                                    style={{
+                                        width: '120px',
                                         height: '31px',
                                         background: 'rgba(255,255,255,.2)',
                                         margin: '16px 24px 16px 0',
-                                        float: 'left'}}/>
-                                <Menu
-                                    theme="dark"
-                                    mode="horizontal"
-                                    // defaultSelectedKeys={['2']}
-                                    style={{ lineHeight: '64px' }}
-                                >
-                                    <Menu.Item key="1">定制服务</Menu.Item>
-                                    <Menu.Item key="2">成品衣物</Menu.Item>
-                                    <Menu.Item key="3">服装配饰</Menu.Item>
-                                    <Menu.Item key="4">专家团队</Menu.Item>
-                                    <Menu.Item key="5">联系我们</Menu.Item>
-                                </Menu>
+                                        float: 'left'
+                                    }} />
+                                <div className="hearderbar">
+                                    <Menu
+                                        theme="dark"
+                                        mode="horizontal"
+                                        // defaultSelectedKeys={['2']}
+                                        style={{ lineHeight: '64px' }}
+                                    >
+                                        <Menu.Item key="1">定制服务</Menu.Item>
+                                        <Menu.Item key="2">成品衣物</Menu.Item>
+                                        <Menu.Item key="3">服装配饰</Menu.Item>
+                                        <Menu.Item key="4">专家团队</Menu.Item>
+                                        <Menu.Item key="5">联系我们</Menu.Item>
+                                    </Menu>
+                                </div>
                             </Header>
 
                         </Layout>
                     </Header>
                     <Content>
                         {/*<Breadcrumb style={{ margin: '16px 0' }}>*/}
-                            {/*<Breadcrumb.Item>Home</Breadcrumb.Item>*/}
-                            {/*<Breadcrumb.Item>List</Breadcrumb.Item>*/}
-                            {/*<Breadcrumb.Item>App</Breadcrumb.Item>*/}
+                        {/*<Breadcrumb.Item>Home</Breadcrumb.Item>*/}
+                        {/*<Breadcrumb.Item>List</Breadcrumb.Item>*/}
+                        {/*<Breadcrumb.Item>App</Breadcrumb.Item>*/}
                         {/*</Breadcrumb>*/}
-                        <div style={{ background: '#fff',
+                        <div style={{
+                            background: '#fff',
                             padding: 24,
                             minHeight: 1000,
-                            backgroundImage: "url("+require("./img/city.png")+")"}}>Content</div>
+                            backgroundImage: "url(" + require("./img/city.png") + ")"
+                        }}>Content</div>
                     </Content>
                     <Footer style={{ textAlign: 'center' }}>
                         Ant Design ©2018 Created by Ant UED
