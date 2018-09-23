b00d3ebd3f80d37704b24e2c1d945440be15aaf1
chengyu <chengyu@singularityclub.net>
2018-09-23 17:31:30 +0800

    门户样式修改

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Icon } from 'antd';
 
+const SubMenu = Menu.SubMenu;
 const { Header, Sider, Content, Footer } = Layout;
 
 class App extends React.Component {
@@ -56,7 +57,7 @@ class App extends React.Component {
                                         width: '120px',
                                         height: '31px',
                                         background: 'rgba(255,255,255,.2)',
-                                        margin: '16px 24px 16px 0',
+                                        margin: '16px 24px 16px 500px',
                                         float: 'left'
                                     }} />
                                 <div className="hearderbar">
@@ -66,11 +67,31 @@ class App extends React.Component {
                                         // defaultSelectedKeys={['2']}
                                         style={{ lineHeight: '64px' }}
                                     >
-                                        <Menu.Item key="1">定制服务</Menu.Item>
-                                        <Menu.Item key="2">成品衣物</Menu.Item>
-                                        <Menu.Item key="3">服装配饰</Menu.Item>
-                                        <Menu.Item key="4">专家团队</Menu.Item>
-                                        <Menu.Item key="5">联系我们</Menu.Item>
+                                        <SubMenu title={<span className="" key="1">定制服务</span>}>
+                                            <Menu.Item key="11">婚礼西服</Menu.Item>
+                                            <Menu.Item key="12">休闲西服</Menu.Item>
+                                            <Menu.Item key="13">商务西服</Menu.Item>
+                                        </SubMenu>
+                                        <SubMenu title={<span className="" key="2">成品衣物</span>}>
+                                            <Menu.Item key="21">婚礼西服</Menu.Item>
+                                            <Menu.Item key="22">休闲西服</Menu.Item>
+                                            <Menu.Item key="23">商务西服</Menu.Item>
+                                        </SubMenu>
+                                        <SubMenu title={<span className="" key="3">服装配饰</span>}>
+                                            <Menu.Item key="31">皮鞋</Menu.Item>
+                                            <Menu.Item key="32">领带</Menu.Item>
+                                            <Menu.Item key="33">伞具</Menu.Item>
+                                        </SubMenu>
+                                        <SubMenu title={<span className="" key="4">专家团队</span>}>
+                                            <Menu.Item key="41">婚礼西服</Menu.Item>
+                                            <Menu.Item key="42">休闲西服</Menu.Item>
+                                            <Menu.Item key="43">商务西服</Menu.Item>
+                                        </SubMenu>
+                                        <SubMenu title={<span className="" key="5">联系我们</span>}>
+                                            <Menu.Item key="51">程昱</Menu.Item>
+                                            <Menu.Item key="52">周林秋</Menu.Item>
+                                            <Menu.Item key="53">王春成</Menu.Item>
+                                        </SubMenu>
                                     </Menu>
                                 </div>
                             </Header>
