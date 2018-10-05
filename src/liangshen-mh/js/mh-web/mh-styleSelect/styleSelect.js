499cdc904a47a950ba5832af0ee6ddb2c5913490
潘康弼 <pankb@singularityclub.net>
2018-10-05 18:13:27 +0800

    ...

---
+++ src/liangshen-mh/js/mh-web/mh-styleSelect/styleSelect.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class StyleSelect extends React.Component {
+    render() {
+        return (
+            <div><div style={{backgroundImage: `url(${BackgroundTwo})` }}>2</div></div>
+        );
+    }
+}
