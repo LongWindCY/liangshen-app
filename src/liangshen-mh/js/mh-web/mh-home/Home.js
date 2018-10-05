2ba6b35205f13fdbd86375a7455e6f9b4a9ebe54
chengyu <chengyu@singularityclub.net>
2018-10-05 12:44:50 +0800

    首页修改

--- src/liangshen-mh/js/mh-web/mh-home/Home.js
+++ src/liangshen-mh/js/mh-web/mh-home/Home.js
@@ -25,118 +25,109 @@ class Home extends React.Component{
                 </Carousel>
                 <Content style={{ margin: '0px', padding: "50px 300px", background: 'black', minHeight: 280 }}>
                     <div className="gutter-example">
-                        <Row gutter={16}>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="婚礼西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="商务西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="休闲西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="韩版西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
+                        <Row>
+                            <Col span={12}><Card
+                                hoverable='true'
+                                style={{ width: 340,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
+                            <Col span={12}><Card
+                                hoverable='true'
+                                style={{ width: 340,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
                         </Row>
                     </div>
                 </Content>
                 <Content style={{ margin: '0px', padding: "50px 300px", background: 'black', minHeight: 280 }}>
                     <div className="gutter-example">
-                        <Row gutter={16}>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="婚礼西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="商务西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="休闲西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
-                            <Col className="gutter-row" span={6}>
-                                <Card
-                                    hoverable='true'
-                                    style={{ width: 240,
-                                        margin: 'auto'}}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta
-                                        title="韩版西服定制"
-                                        description="www.instagram.com"
-                                    />
-                                </Card>
-                            </Col>
+                        <Row>
+                            <Col span={8}><Card
+                                hoverable='true'
+                                style={{ width: 240,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
+                            <Col span={8}><Card
+                                hoverable='true'
+                                style={{ width: 240,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
+                            <Col span={8}><Card
+                                hoverable='true'
+                                style={{ width: 240,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
                         </Row>
+                    </div>
+                </Content>
+                <Content style={{ margin: '0px', padding: "50px 300px", background: 'black', minHeight: 280 }}>
+                    <div className="gutter-example">
+                        <Row>
+                            <Col span={8}><Card
+                                hoverable='true'
+                                style={{ width: 240,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
+                            <Col span={8}><Card
+                                hoverable='true'
+                                style={{ width: 240,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
+                            <Col span={8}><Card
+                                hoverable='true'
+                                style={{ width: 240,
+                                    margin: 'auto'}}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title="婚礼西服定制"
+                                    description="www.instagram.com"
+                                />
+                            </Card></Col>
+                        </Row>
+
                         <div className="button" style={{textAlign: 'center', marginTop: '50px'}}>
                             <Button type="primary" ghost>开启您的专业定制</Button>
                         </div>
