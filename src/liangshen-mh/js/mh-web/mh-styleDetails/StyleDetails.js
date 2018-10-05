cadd7d0f5cb61cf0ba736cf2bb9962d2e64f97a1
王春成 <673047374@qq.com>
2018-10-05 17:15:34 +0800

    样式详情

--- src/liangshen-mh/js/mh-web/mh-styleDetails/StyleDetails.js
+++ src/liangshen-mh/js/mh-web/mh-styleDetails/StyleDetails.js
@@ -7,38 +7,184 @@ class StyleDetails extends React.Component {
   render () {
     return (
       <div >
-        <Content style={{ margin: '0px', padding: "50px 250px", minHeight: 550, }}>
-                    <Row gutter={16}>
-                        <Col className="gutter-row" span={12}>
-                            <Card style={{ width: 390 ,height:370,background: '#F0F2F5', border:0}}>
-                                <img src={URL}/>
-                            </Card>
-                        </Col>
-                        <Col className="gutter-row" span={6}>
-                            <Card style={{ width: 360 ,height:370,background: '#F0F2F5', border:0}}>
-                                <h1>藏青色平纹礼盒</h1>
-                                <hr style={{marginBottom:20}}/>
-                                <span>Lorem ipsum dolor sit amet, consectetur 
-                                adipiscing elit. Aenean euismod bibendum 
-                                laoreet. Proin gravida dolor sit amet lacus 
-                                accumsan et viverra justo commodo. Proin 
-                                sodales pulvinar sic tempor. Sociis natoque 
-                                penatibus et magnis dis parturient montes, 
-                                nascetur ridiculus mus. Nam fermentum, nulla 
-                                luctus pharetra vulputate, felis tellus mollis orci, 
-                                sed rhoncus pronin sapien nunc accuan eget.</span><p/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <Button type="primary">点击定制</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <Button>点击咨询</Button>
-                            </Card>
-                        </Col>
-                        
-                    </Row>
-                </Content>
-                <Content style={{ margin: '0px', paddingLeft: "400px", minHeight: 200,textAlign: 'center',display:'flex' }}>
-                    <hr style={{width:100,}}/>
-                    <h1>定制流程</h1>
-                    <hr style={{width:100}}/>
-                </Content>
+        <Content style={{ margin: '0px', padding: "50px 250px", minHeight: 450, }}>
+            <Row gutter={16}>
+                <Col className="gutter-row" span={12}>
+                    <Card style={{ width: 390 ,height:370,background: '#F0F2F5', border:0}}>
+                        <img src={URL}/>
+                    </Card>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                    <Card style={{ width: 360 ,height:370,background: '#F0F2F5', border:0}}>
+                        <h1>藏青色平纹礼盒</h1>
+                        <hr style={{marginBottom:20}}/>
+                        <span>Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit. Aenean euismod bibendum 
+                        laoreet. Proin gravida dolor sit amet lacus 
+                        accumsan et viverra justo commodo. Proin 
+                        sodales pulvinar sic tempor. Sociis natoque 
+                        penatibus et magnis dis parturient montes, 
+                        nascetur ridiculus mus. Nam fermentum, nulla 
+                        luctus pharetra vulputate, felis tellus mollis orci, 
+                        sed rhoncus pronin sapien nunc accuan eget.</span><p/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Button type="primary">点击定制</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Button>点击咨询</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Content>
+        <Content style={{ margin: '0px', paddingLeft: "300px", minHeight: 60,textAlign: 'center',display:'flex' }}>
+            <span style={{width:180,margin:'15px 50px'}}><hr /></span>
+            <h1>定制流程</h1>
+            <span style={{width:180,margin:'15px 50px'}}><hr /></span><br/>
+        </Content>
+        <Content style={{textAlign: 'center'}}>
+            <span >先这样，再这样，然后这样，最后这样，就行了</span>
+        </Content>
+        <Content style={{ margin: '0px', padding: "50px 100px", minHeight: 250, }}>
+            <Row gutter={16}>
+                <Col className="gutter-row" span={9}>
+                    <Card style={{ 
+                        width: 200 ,
+                        height:200,
+                        marginTop:30,
+                        background: '#F0F2F5', 
+                        border:0, 
+                        borderRight:'1px solid',
+                        display:'flex',
+                        alignItems:'center',
+                        justifyContent: 'center'
+                        }}>
+                        <h1>面料选择</h1>
+                    </Card>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                    <Card style={{ width: 360 ,height:300,background: '#F0F2F5', border:0}}>
+                        <span>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Aenean euismod bibendum laoreet. 
+                            Proin gravida dolor sit amet lacus 
+                            accumsan et viverra justo commodo. 
+                            Proin sodales pulvinar sic tempor. 
+                            Sociis natoque penatibus et 
+                            magnis dis parturient montes, 
+                            nascetur ridiculus mus. 
+                            Nam fermentum, nulla luctus 
+                            pharetra vulputate, felis 
+                            tellus mollis orci, sed rhoncus 
+                            pronin sapien nunc accuan eget.</span>
+                    </Card>
+                </Col>
+            </Row>
+        </Content>
+        <Content style={{ margin: '0px', padding: "50px 100px", minHeight: 250, }}>
+            <Row gutter={16}>
+                <Col className="gutter-row" span={9}>
+                    <Card style={{ 
+                        width: 200 ,
+                        height:200,
+                        marginTop:30,
+                        background: '#F0F2F5', 
+                        border:0, 
+                        borderRight:'1px solid',
+                        display:'flex',
+                        alignItems:'center',
+                        justifyContent: 'center'
+                        }}>
+                        <h1>量身尺寸</h1>
+                    </Card>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                    <Card style={{ width: 360 ,height:300,background: '#F0F2F5', border:0}}>
+                        <span>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Aenean euismod bibendum laoreet. 
+                            Proin gravida dolor sit amet lacus 
+                            accumsan et viverra justo commodo. 
+                            Proin sodales pulvinar sic tempor. 
+                            Sociis natoque penatibus et 
+                            magnis dis parturient montes, 
+                            nascetur ridiculus mus. 
+                            Nam fermentum, nulla luctus 
+                            pharetra vulputate, felis 
+                            tellus mollis orci, sed rhoncus 
+                            pronin sapien nunc accuan eget.</span>
+                    </Card>
+                </Col>
+            </Row>
+        </Content>
+        <Content style={{ margin: '0px', padding: "50px 100px", minHeight: 250, }}>
+            <Row gutter={16}>
+                <Col className="gutter-row" span={9}>
+                    <Card style={{ 
+                        width: 200 ,
+                        height:200,
+                        marginTop:30,
+                        background: '#F0F2F5', 
+                        border:0, 
+                        borderRight:'1px solid',
+                        display:'flex',
+                        alignItems:'center',
+                        justifyContent: 'center'
+                        }}>
+                        <h1>细节挑选</h1>
+                    </Card>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                    <Card style={{ width: 360 ,height:300,background: '#F0F2F5', border:0}}>
+                        <span>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Aenean euismod bibendum laoreet. 
+                            Proin gravida dolor sit amet lacus 
+                            accumsan et viverra justo commodo. 
+                            Proin sodales pulvinar sic tempor. 
+                            Sociis natoque penatibus et 
+                            magnis dis parturient montes, 
+                            nascetur ridiculus mus. 
+                            Nam fermentum, nulla luctus 
+                            pharetra vulputate, felis 
+                            tellus mollis orci, sed rhoncus 
+                            pronin sapien nunc accuan eget.</span>
+                    </Card>
+                </Col>
+            </Row>
+        </Content>
+        <Content style={{ margin: '0px', padding: "50px 100px", minHeight: 250, }}>
+            <Row gutter={16}>
+                <Col className="gutter-row" span={9}>
+                    <Card style={{ 
+                        width: 200 ,
+                        height:200,
+                        marginTop:30,
+                        background: '#F0F2F5', 
+                        border:0, 
+                        borderRight:'1px solid',
+                        display:'flex',
+                        alignItems:'center',
+                        justifyContent: 'center'
+                        }}>
+                        <h1>专业建议</h1>
+                    </Card>
+                </Col>
+                <Col className="gutter-row" span={6}>
+                    <Card style={{ width: 360 ,height:300,background: '#F0F2F5', border:0}}>
+                        <span>Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit. 
+                            Aenean euismod bibendum laoreet. 
+                            Proin gravida dolor sit amet lacus 
+                            accumsan et viverra justo commodo. 
+                            Proin sodales pulvinar sic tempor. 
+                            Sociis natoque penatibus et 
+                            magnis dis parturient montes, 
+                            nascetur ridiculus mus. 
+                            Nam fermentum, nulla luctus 
+                            pharetra vulputate, felis 
+                            tellus mollis orci, sed rhoncus 
+                            pronin sapien nunc accuan eget.</span>
+                    </Card>
+                </Col>
+            </Row>
+        </Content>
       </div>
     )
   }
